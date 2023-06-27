a77c6b8a3ba14f829b0bee3dacef143e92a0d508
namous68 <vnesys@gmail.com>
2023-06-27 16:08:02 +0200

    php

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+
+
+$lastname ='ahmed';
+$firstname = 'namo';
+$age = '42';
+
+echo $lastname;
+echo '<br/>';
+echo $firstname;
+echo '<br/>';
+echo $age;
+echo '<br/>';
+ 
+$km = '1';
+echo $km;
+$km = '6';
+echo $km;
+echo '<br/>';
+$km = 6;
+echo $km;
+echo '<br/>';
+$km = '6';
+echo ($km*3);
+echo '<br/>';
+$km = '125';
+echo $km;
+echo '<br/>';
+$saluttaion ='bonjour';
+echo $variable;
+$age = 30;
+$salaire = 1500.50;
+$estActif = true;
+echo '<br/>';
+echo "salutation: " . $saluttaion . "<br>";
+echo "Âge : " . $age . "<br>";
+echo "Salaire : " . $salaire . "<br>";
+echo "Est actif : " . ($estActif ? "Oui" : "Non") . "<br>";
+echo'<br/>';
+$valeur = '';
+echo $valeur;
+echo'<br/>';
+$valeur = '33';
+echo $valeur;
+echo'<br/>';
+$name = 'ahmed';
+echo 'Bonjour  '  . $name .' , comment vas tu ?';
+echo'<br/>';
+$lastname ='namou';
+$firstname = 'ahmed';
+$age = '42';
+echo 'bonjour ' . $lastname .  $firstname . ' , tu as' . $age . 'ans';
+echo'<br/>';
+echo'<br/>';
+$result1; 
+$result2;
+$result3;
+$result1 = 3+4;
+$result2 =5*20;
+$result3=45/5;
+echo $result1;
+echo'<br/>';
+echo $result2;
+echo'<br/>';
+echo $result3;
+echo'<br/>';
+
+
+
+
+
+
+
+
+
+
+
+
+?>
